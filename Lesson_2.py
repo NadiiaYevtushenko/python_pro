37aa35a4f16c1c7f047de494b457ee59475080b8
NadiiaYevtushenko <nedilja90@gmail.com>
2024-04-27 22:23:27 +0300

    Changes

---
+++ Lesson_2.py
@@ -0,0 +1,8 @@
+# 2. Типи даних
+# цій лекції ми розглянемо основні типи даних в Python 3 та їх використання. Ви дізнаєтеся про числові типи (int, float, complex), рядкові дані (str), логічний тип (bool) та колекції, такі як списки, кортежі, множини та словники. Основні операції та приклади використання кожного типу даних допоможуть вам краще розібратися з їхнім використанням в програмуванні. Запрошуємо до ознайомлення!
+# Корисні посилання:
+# 1. [Документація Python](https://docs.python.org/3/)
+# 2. [Real Python - Python Data Types](https://realpython.com/python-data-types/)
+# 3. [GeeksforGeeks - Python Data Types](https://www.geeksforgeeks.org/python-data-types/)
+# 4. [Python - Built-in Types](https://docs.python.org/3/library/stdtypes.html)
+# 5. [W3Schools - Python Data Types](https://www.w3schools.com/python/python_datatypes.asp)
---
+++ Lesson_3.py
@@ -0,0 +1,13 @@
+# 3. Вступ до ООП
+# 1. **Python Official Documentation:**
+#   - [Python 3 Documentation - Classes](https://docs.python.org/3/tutorial/classes.html)
+#   - [Python 3 Documentation - Object-Oriented Programming](https://docs.python.org/3/tutorial/classes.html)
+# 2. **Real Python:**
+#   - [Object-Oriented Programming (OOP) in Python 3](https://realpython.com/python3-object-oriented-programming/)
+#   - [Inheritance and Composition: A Python OOP Guide](https://realpython.com/inheritance-composition-python/)
+# 3. **GeeksforGeeks:**
+#   - [Python - Object-Oriented Programming](https://www.geeksforgeeks.org/object-oriented-programming-in-python-set-1-class-and-its-members/)
+#   - [Python - Inheritance](https://www.geeksforgeeks.org/inheritance-in-python/)
+# 4. **W3Schools:**
+#   - [Python Classes and Objects](https://www.w3schools.com/python/python_classes.asp)
+#   - [Python Inheritance](https://www.w3schools.com/python/python_inheritance.asp)
